60ddb96e4f0a889d450cc1e2c1961fec18ffee3b
giuliadinardo1 <66077960+giuliadinardo1@users.noreply.github.com>
2021-12-04 00:56:10 -0500

    assignment4

--- Assignment4.m
+++ Assignment4.m
@@ -1,1 +1,29 @@
-hello peoples
+%i 
+data = load('EEGdata_assignment4.mat');
+
+EEG = data.EEGa4;
+Fs = data.Fs;
+
+
+[eeg_mag_1, eeg_ph_1, eeg_f_1] = fourier_dt(EEG, Fs, 'half');
+
+figure(1)
+plot(eeg_f_1, eeg_mag_1)
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+
+figure(2)
+plot(eeg_f_1, eeg_mag_1)
+
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+xlim([55 65])
+
+y_filtered_mag_spec = filter (Hd, 1, eeg_mag_1);
+
+figure(3)
+plot(eeg_f_1, y_filtered_mag_spec)
+
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+xlim([55 65])
